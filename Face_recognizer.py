29bd32d5d537828df2daea34a4ffdeb941c112f2
Yeswanth Sai Inturi <yeswanthsai.inturi@gmail.com>
2022-05-13 04:40:11 +0530

    final_commit

---
+++ Face_recognizer.py
@@ -0,0 +1,71 @@
+import numpy
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+path = 'Resources'
+images = []
+classNames = []
+myList = os.listdir(path)
+# print(myList)
+
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+# print(classNames)
+
+
+def findEncodings(images):
+    encodeList = []
+    for imgF in images:
+        imgF = cv2.cvtColor(imgF, cv2.COLOR_BGR2RGB)
+        encodeFunc = face_recognition.face_encodings(imgF)[0]
+        encodeList.append(encodeFunc)
+    return encodeList
+
+
+def markAttendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name}, {dtString}')
+
+
+encodeListKnown = findEncodings(images)
+print('Encoding Completed.')
+
+cap = cv2.VideoCapture(0)
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodeCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDist = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print(faceDist)
+        matchIndex = numpy.argmin(faceDist)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            # print(name)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            markAttendance(name)
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
